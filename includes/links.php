efec1214bfc52fce426bd31a997f709261e44455
ICARUS343 <31902120+ICARUS343@users.noreply.github.com>
2018-02-27 09:06:56 -0500

    FlexSlider
    1.Added bootsstrap plugin.
    2.Moved all the links to a php file in includes and used a php include to add in the index file.
    3. Changed the name of the css file to ovverride.css and moved it to the bootstrap.css file as override.css.
    4. Added a flexslider.js file in js folder.

---
+++ includes/links.php
@@ -0,0 +1,8 @@
+<link rel="stylesheet" type="text/css" href="./bootstrap/css/bootstrap.min.css">
+<link rel="stylesheet" href="./bootstrap/css/custom.css">
+<link rel="stylesheet" type="text/css" href="./bootstrap/css/navigation.css">
+<link rel="stylesheet" type="text/css" href="./bootstrap/css/responsiveslides.css">
+<link rel="stylesheet" type="text/css" href="https://fonts.googleapis.com/css?family=Montserrat:400,700">
+     <link rel="stylesheet" href="./bootstrap/css/override.css">
+ 
+  
--- index.php
+++ index.php
@@ -3,54 +3,39 @@
 
 <div class="container">
 <head>
+  <?php 
+ 	include'includes/links.php';
+ 	?>
+	<meta charset="utf-8">
 
-	<link rel="stylesheet" href="css/flexslider.css" type="text/css">
-
-	<!-- google font link -->
-	<link href="https://fonts.googleapis.com/css?family=Cinzel|Lato" rel="stylesheet">
-	<!-- link to style.css -->
-	<link rel="stylesheet" type="text/css" href="css/style.css">
-
-	
-
-	<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.6.2/jquery.min.js"></script>
-	<script src="jquery_plugin/jquery.flexslider.js"></script>	
-    <script>
-    $(window).load(function() {
-    $('.flexslider').flexslider();
-    });
-    </script>
 
 	<title>Computer Science Students in CSC174</title>
 </head>
 
 <body>
-	<!--TO CODER: Please implement php code to create a menu highlighter for the current page-->
+	<!--Done--><!--TO CODER: Please implement php code to create a menu highlighter for the current page-->
 	<!--TO Design Artist: Make the navbar look better and easier to use. Maybe have them be horizontal, right aligned, with the city name left aligned. The city name can act as a logo, and also linked to the home page. Also please add css for when hovering over nav elements.-->
-	<?php 
-	include 'includes/navigation.php';
-	 include 'includes/script.php';
-	 ?>
+	<?php include 'includes/navigation.php';?>
+	
+	<?php include 'includes/script.php';?>
+	
 	<h1>Computer Science Students in CSC174</h1>
 	<div>
 		<p>This page is dedicated to three students that all study Computer Science at the University of Rochester and all three also happen to be taking the course CSC174 - Advanced Front-End Web Design, in Spring 2018. You can learn more about the individual students, their academic and professional careers on their individual pages.</p>
 	</div>
 
-	<!-- TO CODER: Please make sure the flexslider is implemented correctly and works-->
+	<!--Done--><!-- TO CODER: Please make sure the flexslider is implemented correctly and works-->
 	<!-- TO Design Artist: Please make the flexslider make better. Maybe make it smaller instead of fullscreen so that the images are not distorted. Place a nice frame around the images that fit well with the rest of the color scheme of your choice-->
-	<div class="flexslider">
-	    <ul class="slides">
-	    <li>
-	      <img src="images/kyle.jpg" alt="picture of Kyle">
-	    </li>
-	    <li>
-	      <img src="images/sid.jpg" alt="picture of Sidhant">
-	    </li>
-	    <li>
-	      <img src="images/face.jpg" alt="picture of Zubair">
-	    </li>
-	    </ul>
-	</div>
+	
+		<div class="container">	
+  		<div class="slides">
+  		<ul class="rslides">
+		<li> <img src="images/kyle.jpg" alt="picture of Kyle"> </li>
+		<li><img src="images/sid.jpg" alt="picture of Sidhant"></li>
+		<li><img src="images/face.jpg" alt="picture of Zubair"> </li>
+		</ul>
+		</div>
+	
    
 	<h2>Kyle Pollock</h2> 
 	<div class="wrap">
@@ -65,9 +50,6 @@
 	<div class="two">
 	<p>My name is Zubair Aziz. I am 21 years old. I was born on April 28, 1996. My hometown is Kuala Lumpur, Malaysia. I am currently a junior, anticipated to graduate in May, 2019.</p>
 </div>
-<?php
-    include 'includes/script.php';
-    ?>
 
 </body>
 <footer>
